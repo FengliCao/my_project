759c77481ea58221831c6b2ee7f5c073e88b9ce9
FengliCao <cflmqtt@163.com>
2018-04-05 22:19:50 +0800

    modified dev_driver -add pwm

--- Dev_driver/motor.c
+++ Dev_driver/motor.c
@@ -11,6 +11,8 @@
 #include <linux/timer.h>
 //#include <stdio.h>
 #include <linux/delay.h>
+#include <linux/hrtimer.h>
+#include <linux/jiffies.h>
 //定义电机脉冲和方向
 #define Motor_Pul_C(x)  gpio_set_value(PAD_GPIO_C+4,x)  //c4
 #define Motor_Pul_B(x)  gpio_set_value(PAD_GPIO_C+8,x)  //c8
@@ -25,6 +27,15 @@
 #define XianWei_B_S  gpio_get_value(PAD_GPIO_B+31) 
 #define XianWei_A_S  gpio_get_value(PAD_GPIO_D+1) 
 
+#define PWM_A_H gpio_set_value(PAD_GPIO_B+28,1) //b28
+#define PWM_A_L gpio_set_value(PAD_GPIO_B+28,0) //b28
+#define PWM_B_H gpio_set_value(PAD_GPIO_B+29,1) //b29
+#define PWM_B_L gpio_set_value(PAD_GPIO_B+29,0) //b29
+
+ktime_t kt;
+static struct hrtimer pwm_timer;
+static int a_value = 150; //pwm duty ratio := (a_value/2000*100%)
+static int b_value = 150; //pwm duty ratio := (b_value/2000*100%)
 //定义高低电平
 const int high = 1;
 const int low = 0;
@@ -66,12 +77,46 @@ static ssize_t  dev_open(struct inode *node, struct file *filp)
 	gpio_direction_input(PAD_GPIO_B+30); 
 	gpio_direction_input(PAD_GPIO_B+31) ;
 	gpio_direction_input(PAD_GPIO_D+1) ;
-
+	
+	//pwm output 
+	gpio_direction_output(PAD_GPIO_B+28,0); //B28
+	gpio_direction_output(PAD_GPIO_B+29,0); //B29
 
 	printk("<0> \n Motor dev_driver open\n");
 
 	return 0;
 }
+
+/*  function  */
+static enum hrtimer_restart hrtimer_handler(struct hrtimer *timer)
+{
+	    static int count = 0;  //count -all[0,2000]  -value[60, 240]
+		count++;
+		count = count % 2000;
+		// A
+		if(count < 60 || count > 240){
+		PWM_A_L;
+		}
+		else if(count >=60 && count <= a_value){
+			PWM_A_H;
+			}
+		else{
+			PWM_A_L;
+		}
+		// B
+		if(count < 60 || count > 240){
+		PWM_B_L;
+		}
+		else if(count >=60 && count <= b_value){
+			PWM_B_H;
+			}
+		else{
+			PWM_B_L;
+		}
+hrtimer_forward(pwm_timer, pwm_timer->base->get_time(), kt);
+return HRTIMER_RESTART;
+}
+
 static long dev_ioctl(struct file *file, unsigned int cmd,unsigned long arg)
 {
 	switch(cmd)
@@ -118,6 +163,18 @@ case 3:
 	Motor_Pul_B(0);
 	Motor_Pul_C(0);
 	break;
+case 7:
+	if(arg < 60 || arg >240)
+		break;
+	else
+		a_value = arg;
+		break;
+case 8:
+	if(arg < 60 || arg >240)
+		break;
+	else
+		b_value = arg;
+		break;
 default : 
 	break;
 	}
@@ -137,7 +194,6 @@ static struct file_operations dev_fops =
 static int dev_init(void)  
 {  	
 
-//  unsigned long delay = jiffies + HZ/500;   
    /*注册设备+创建设备文件*/
     cdev_init(&cdev,&dev_fops);
     alloc_chrdev_region(&devno, 0 , 1 , "motor");
@@ -146,7 +202,13 @@ static int dev_init(void)
     myclass = class_create(THIS_MODULE,"motor");
     myclass_dev = device_create(myclass,NULL,devno,0,"motor");
 
-    printk("<0>\n 欢迎使用motor_contral! \n"); 
+   kt = ktime_set(0, 10000); /* 0 sec, 10000 nsec */
+   hrtimer_init(&pwm_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+   //hrtimer_set_expires(&timer, kt);
+   hrtimer_start(&pwm_timer, kt, HRTIMER_MODE_REL);
+   timer.function = hrtimer_handler;
+
+	printk("<0>\n 欢迎使用motor_contral! \n"); 
 return 0;
 }  
 
@@ -157,6 +219,7 @@ static void dev_exit(void)
 	unregister_chrdev_region(devno,1);
 	device_unregister(myclass_dev);
 	class_destroy(myclass);
+	hrtimer_cancel(&pwm_timer);
 	gpio_free(PAD_GPIO_C+4);
     printk("<0> \n Goodbye!\n");  
 }  
